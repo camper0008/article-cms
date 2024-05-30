c3a5c1fc048a9768a3891c868b8a8c7c6057e2cf
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2024-05-30 13:03:32 +0200

    begin file db impl

--- src/db/file_db.ts
+++ src/db/file_db.ts
@@ -1,21 +1,53 @@
-import { Db, Id, Post, Result } from "./mod.ts";
+import { join } from "https://deno.land/std@0.224.0/path/mod.ts";
+import { Db, Id, Post, Result, SortedBy } from "./mod.ts";
+
+function new_post_position(
+    posts: Post[],
+    new_post: Post[],
+    sorted_by: SortedBy,
+): number {
+    if (sorted_by === "none") {
+        return new_post.length;
+    } else if (sorted_by === "created")
+        throw new Error("Method not implemented.");
+    }
+}
 
 export class FileDb implements Db {
-    private path: string;
-    constructor(path: string) {
-        this.path = path;
+    private directory: string;
+    constructor(directory: string) {
+        const info = Deno.statSync(directory);
+        if (!info.isDirectory) {
+            throw new Error(`Path ${directory} is not a valid directory`);
+        }
+        this.directory = directory;
     }
+    async posts(
+        offset: number,
+        limit: number,
+        sorted_by: SortedBy,
+    ): Promise<Result<Post[], string>> {
+        const result = [];
+        const files = Deno.readDir(this.directory);
+        for await (const file of files) {
+            const content = await Deno.readTextFile(
+                join(this.directory, file.name),
+            );
+            const obj: Post = JSON.parse(content);
+            if (result.length < limit) {
+                result.push(obj);
+            }
+        }
 
-    posts(offset: number, limit: number): Result<Post[], string> {
         throw new Error("Method not implemented.");
     }
-    create_post(post: Omit<Post, "id">): Result<Id, string> {
+    async create_post(post: Omit<Post, "id">): Promise<Result<string, string>> {
         throw new Error("Method not implemented.");
     }
-    update_post(post: Post): Result<null, string> {
+    async update_post(post: Post): Promise<Result<null, string>> {
         throw new Error("Method not implemented.");
     }
-    delete_post(id: string): Result<null, string> {
+    async delete_post(id: Id): Promise<Result<null, string>> {
         throw new Error("Method not implemented.");
     }
 }
--- src/db/mod.ts
+++ src/db/mod.ts
@@ -12,9 +12,15 @@ export interface Post {
 
 export type Result<T, E> = { ok: true; value: T } | { ok: false; error: E };
 
+export type SortedBy = "created" | "none";
+
 export interface Db {
-    posts(offset: number, limit: number): Result<Post[], string>;
-    create_post(post: Omit<Post, "id">): Result<Id, string>;
-    update_post(post: Post): Result<null, string>;
-    delete_post(id: Id): Result<null, string>;
+    posts(
+        offset: number,
+        limit: number,
+        sorted_by: SortedBy,
+    ): Promise<Result<Post[], string>>;
+    create_post(post: Omit<Post, "id">): Promise<Result<Id, string>>;
+    update_post(post: Post): Promise<Result<null, string>>;
+    delete_post(id: Id): Promise<Result<null, string>>;
 }
